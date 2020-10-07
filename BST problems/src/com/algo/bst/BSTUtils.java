e583600a605b66275c93e51f01c82c81e2b25027
vardhanskale <vardhanskale@gmail.com>
2020-10-07 13:53:43 +0530

    Add files via upload

--- BST problems/src/com/algo/bst/BSTUtils.java
+++ BST problems/src/com/algo/bst/BSTUtils.java
@@ -1,9 +1,12 @@
 package com.algo.bst;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Random;
 
 /*
  *  In Binary tree, every node has at most two child nodes. 
@@ -69,6 +72,36 @@ public class BSTUtils {
 	  return root ; 	  
 	} 		  
 
+	// Build a random BST
+	public static BSTNode buildRBST(int n) {
+		int[] in = new int[n];
+		Random r = new Random();
+		HashSet<Integer> set = new HashSet<Integer>();
+		for (int i = 0; i < n;) {
+			int tmp = r.nextInt(2 * n) + 1;
+			if (!set.contains(tmp)) {
+				in[i++] = tmp;
+				set.add(tmp);
+			}
+		}
+		Arrays.sort(in);
+		return auxBuildRBST(in, 0, n - 1);
+	}
+
+	// TC:Theta(n) SC:O(log n)
+	private static BSTNode auxBuildRBST(int[] in, int l, int r) {
+		if (l > r)
+			return null;
+		if (l == r)
+			return new BSTNode(in[l], 0);
+		int m = (l + r) / 2;
+		BSTNode root = new BSTNode(in[m], m-l);
+		root.left = auxBuildRBST(in, l, m - 1);
+		root.right = auxBuildRBST(in, m + 1, r);
+		return root;
+	}
+
+	
 	/*
 	public static TreeNode createBBST(int n) {
 		int[] in = new int[n];
