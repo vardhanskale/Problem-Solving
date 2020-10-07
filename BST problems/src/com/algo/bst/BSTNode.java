8ede1a1566a88ba1e54bc6bc99cb166da235b3ad
vardhanskale <vardhanskale@gmail.com>
2020-10-07 09:48:36 +0530

    Add files via upload

---
+++ BST problems/src/com/algo/bst/BSTNode.java
@@ -0,0 +1,19 @@
+package com.algo.bst;
+
+public class BSTNode {
+	
+	BSTNode left;
+	BSTNode right;
+	int data;
+	public int lst_size;
+
+	public BSTNode() {
+
+	}
+
+	public BSTNode(int data) {
+		this.data = data;
+	}
+
+
+}
